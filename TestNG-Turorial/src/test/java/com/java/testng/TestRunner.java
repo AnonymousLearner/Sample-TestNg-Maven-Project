13ea6370af60954bd0b87ab42bab7186abadcde6
lakshmi <lakshmi@lakshmis-mbp.attlocal.net>
2017-12-15 14:36:42 -0500

    Initial-Verision

---
+++ TestNG-Turorial/src/test/java/com/java/testng/TestRunner.java
@@ -0,0 +1,12 @@
+package com.java.testng;
+
+import org.testng.annotations.Test;
+
+public class TestRunner 
+{
+  @Test
+  public void TestCaseOne() 
+  {
+	  System.out.println("TestCase One");
+  }
+}
